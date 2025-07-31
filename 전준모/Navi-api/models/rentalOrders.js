88ef3ba2ea21fb7ce424dccbe788eb9ba3d01b84
aggawa <whyd_o@naver.com>
2025-07-31 18:21:29 +0900

    up

---
+++ 전준모/Navi-api/models/rentalOrders.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+
+module.exports = class RentalOrders extends Sequelize.Model {
+   static init(sequelize) {
+      return super.init(
+         {
+            orderStatus: {
+               type: Sequelize.STRING(255),
+               allowNull: false,
+            },
+         },
+         {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'RentalOrder',
+            tableName: 'rentalOrders',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+         }
+      )
+   }
+
+   static associate(db) {}
+}
